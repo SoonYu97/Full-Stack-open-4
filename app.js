4ad9c9311739b891e2a5dee158b5cf648c07164c
Soon Yu <yusoon.97417@gmail.com>
2023-02-18 22:23:28 +0800

    Exercise 4.3

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const blogsRouter = require("./controllers/blogs");
-const config = require("./utils/config");
-const middleware = require("./utils/middleware");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI)
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use("/api/blogs", blogsRouter);
+app.use('/api/blogs', blogsRouter)
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = app;
+module.exports = app
