1eef7e66642104164a06f89b546e29310a5b86bd
Soon Yu <yusoon.97417@gmail.com>
2023-02-19 10:15:21 +0800

    Exercise 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -13,19 +13,42 @@ beforeEach(async () => {
 	await Promise.all(promiseArray)
 })
 
-test('blogs are returned as json', async () => {
-	await api
-		.get('/api/blogs')
-		.expect(200)
-		.expect('Content-Type', /application\/json/)
-})
+describe('blog ', () => {
+	test('are returned as json', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
 
-test('blog posts have a unique identifier property named id', async () => {
-	const blogs = await helper.blogsInDb()
-	expect(blogs[0].id).toBeDefined()
-	expect(blogs[0]._id).toBeUndefined()
-})
+	test('have a unique identifier property named id', async () => {
+		const blogs = await helper.blogsInDb()
+		expect(blogs[0].id).toBeDefined()
+		expect(blogs[0]._id).toBeUndefined()
+	})
 
+	test('can be added', async () => {
+		const newBlog = {
+			title: 'Nextjs patterns',
+			author: 'Michael Chan',
+			url: 'https://nextjs.org/',
+			likes: 17,
+		}
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
+
+		const response = await api.get('/api/blogs')
+
+		const titles = response.body.map(r => r.title)
+		expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+		expect(titles).toContain(
+			'Nextjs patterns'
+		)
+	})
+})
 afterAll(async () => {
 	await mongoose.connection.close()
 })
