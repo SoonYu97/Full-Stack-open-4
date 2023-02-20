85c50e4132efe96b6e7af0ec56c3ed386907f6bd
Soon Yu <yusoon.97417@gmail.com>
2023-02-20 20:52:32 +0800

    Exercise 5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,44 +9,37 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
 	const user = request.user
-
-	const body = request.body
-
-	if (body['title'] && body['url']) {
-		const blog = new Blog({
-			title: body.title,
-			url: body.url,
-			author: body.author || '',
-			likes: body.likes || 0,
-			user: user.id,
-		})
-		const savedBlog = await blog.save()
-
-		user.blogs = user.blogs.concat(savedBlog._id)
-		await user.save()
-		return response.status(201).json(savedBlog)
+	if (!user) {
+		return response.status(401).json({ error: 'operation not permitted' })
 	}
-	response.status(400).end()
+	
+	const { title, author, url, likes } = request.body
+	const blog = new Blog({
+		title, author, url, 
+		likes: likes ? likes : 0
+	})
+	blog.user = user._id
+	
+	const createdBlog = await blog.save()
+
+	user.blogs = user.blogs.concat(createdBlog._id)
+	await user.save()
+	
+	response.status(201).json({
+		...(createdBlog.toObject()),
+		id: createdBlog._id,
+		user: {id: user.id, name:user.name, username:user.username}
+	})
 })
 
-blogsRouter.put('/:id', middleware.userExtractor, async (request, response) => {
-	const user = request.user
-
-	const blog = await Blog.findById(request.params.id)
-
-	if (blog.user.toString() === user.id.toString()) {
-		await Blog.findByIdAndUpdate(request.params.id)
-		const updatedBlog = await Blog.findByIdAndUpdate(
-			request.params.id,
-			request.body,
-			{ new: true }
-		)
-		return response.json(updatedBlog)
-	} else {
-		return response.status(401).json({
-			error: 'invalid username or password',
-		})
-	}
+blogsRouter.put('/:id', async (request, response) => {
+	await Blog.findByIdAndUpdate(request.params.id)
+	const updatedBlog = await Blog.findByIdAndUpdate(
+		request.params.id,
+		request.body,
+		{ new: true }
+	)
+	return response.json(updatedBlog)
 })
 
 blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
